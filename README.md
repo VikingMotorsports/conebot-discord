a5fa474a5d599c46c50e05381b1bc9d17a53fc83
gearysw <46032052+gearysw@users.noreply.github.com>
2019-11-16 23:27:21 -0800

    code started

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Cone Bot for Discord
 
 Functionally identical to [Orange Cone](https://github.com/gearysw/orangecone), adapted for use in Discord. Uses the [Discord.js](https://discord.js.org/#/) library.
+
+This bot uses the `dotenv` Node module to load environment variables. It uses a `TOKEN` key to login to Discord.
---
+++ bot.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const botSettings = require('./botSettings.json');
+const bot = new Discord.Client();
+
+bot.login(botSettings.token);
+
+bot.on('ready', async () => {
+    console.log(`Bot online as ${bot.user.username}`);
+    try {
+        const invite = await bot.generateInvite(['ADD_REACTIONS', 'SEND_MESSAGES', 'VIEW_CHANNEL', 'EMBED_LINKS', 'ATTACH_FILES', 'READ_MESSAGE_HISTORY', 'CONNECT', 'SPEAK', 'CHANGE_NICKNAME', 'MANAGE_ROLES', 'MANAGE_EMOJIS']);
+        console.log(invite);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+bot.on('guildMemberAdd', async (member) => {
+
+});
+
+bot.on('message', async (message) => {
+    // console.log(message);
+
+});
