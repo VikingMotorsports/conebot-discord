6c2e306801a5511e651f0cbb9ca53d23e6af4a14
gearysw <46032052+gearysw@users.noreply.github.com>
2019-12-10 00:22:31 -0800

    removed @everyone from list of roles

--- cmds/role.js
+++ cmds/role.js
@@ -12,7 +12,7 @@ module.exports = {
 
         if (!args.length) {
             const Roles = message.guild.roles.map(r => r.name);
-            const miscRoles = ['Cone Bot', 'Member', 'Bot', 'Overlord'];
+            const miscRoles = ['Cone Bot', 'Member', 'Bot', 'Overlord', '@everyone'];
             const leadershipRoles = ['Admin', 'Leadership', 'Subsystem Lead'];
             const addableRoles = Roles.filter(a => !miscRoles.includes(a) && !leadershipRoles.includes(a));
 
