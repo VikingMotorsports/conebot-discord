1041fff94f38c795fceaff5f953fe4dce57eeaa5
gearysw <46032052+gearysw@users.noreply.github.com>
2019-12-16 20:13:41 -0800

    added email database and streamlined code

---
+++ cmds/email.js
@@ -0,0 +1,53 @@
+const {
+    prefix
+} = require('../config.json');
+const fs = require('fs');
+
+module.exports = {
+    name: 'email',
+    aliases: ['emails', 'mail'],
+    description: `Stores your email and display it upon calling \`${prefix}email\` by itself`,
+    args: false,
+    usage: '<email address> or @username',
+    easteregg: false,
+    execute: async (message, args) => {
+        if (!args.length) {
+            const name = (!message.member.nickname) ? message.author.username : message.member.nickname;
+            fs.readFile('./emails.json', (err, data) => {
+                if (err) throw err;
+                const json = JSON.parse(data);
+                const email = json[message.author.id];
+                if (!email) return message.channel.send(`No email found for ${name}.`);
+                message.channel.send(`${name}'s email address: ${email}`);
+            });
+            return;
+        }
+        if (message.mentions.users.size > 0) {
+            message.mentions.members.map(user => {
+                const name = (!user.nickname) ? user.user.username : user.nickname;
+                fs.readFile('./emails.json', (err, data) => {
+                    if (err) throw err;
+                    const json = JSON.parse(data);
+                    const email = json[user.id];
+                    if (!email) return message.channel.send(`No email found for ${name}.`);
+                    message.channel.send(`${name}'s email address: ${email}`);
+                });
+            });
+            return;
+        }
+        if (args.length && args[0].match(/^\w+[\w-\.]*\@\w+((-\w+)|(\w*))\.[a-z]{2,3}$/)) {
+            fs.readFile('./emails.json', (err, data) => {
+                if (err) throw err;
+                let json = JSON.parse(data);
+                json[message.author.id] = args[0];
+                fs.writeFile('./emails.json', JSON.stringify(json), err => {
+                    if (err) throw err;
+                    message.channel.send('Email saved.');
+                });
+            });
+            return;
+        } else {
+            message.channel.send('Please input a valid email address.');
+        }
+    }
+}
--- cmds/phone.js
+++ cmds/phone.js
@@ -8,7 +8,7 @@ module.exports = {
     aliases: ['number', 'phonenumber', 'contact'],
     description: `Stores your phone number and display it upon calling \`${prefix}phone\` by itself`,
     args: false,
-    usage: '123-456-7890',
+    usage: '123-456-7890 or @username',
     easteregg: false,
     execute: async (message, args) => {
         if (!args.length) {
@@ -17,22 +17,24 @@ module.exports = {
                 if (err) throw err;
                 const json = JSON.parse(data);
                 const number = json[message.author.id];
-                if (!number) return message.channel.send('No phone number found.');
+                if (!number) return message.channel.send(`No phone number found for ${name}.`);
                 message.channel.send(`${name}'s phone number: ${number}`);
             });
+            return;
         }
 
         if (message.mentions.users.size > 0) {
-            const mentionList = message.mentions.members.map(user => {
+            message.mentions.members.map(user => {
                 const name = (!user.nickname) ? user.user.username : user.nickname;
                 fs.readFile('./phones.json', (err, data) => {
                     if (err) throw err;
                     const json = JSON.parse(data);
                     const number = json[user.id];
-                    if (!number) return message.channel.send('No phone number found.');
-                    return message.channel.send(`${name}'s phone number: ${number}`);
+                    if (!number) return message.channel.send(`No phone number found for ${name}.`);
+                    message.channel.send(`${name}'s phone number: ${number}`);
                 });
             });
+            return;
         }
 
         if (args.length && args[0].match(/^[2-9]\d{2}-\d{3}-\d{4}$/)) {
@@ -45,7 +47,8 @@ module.exports = {
                     message.channel.send('Phone number saved.');
                 });
             });
-        } else if (args.length && message.mentions.users.size === 0 && !args[0].match(/^[2-9]\d{2}-\d{3}-\d{4}$/)) {
+            return
+        } else {
             message.channel.send('Please input your phone number with the following format: `123-456-7890`');
         }
     }
