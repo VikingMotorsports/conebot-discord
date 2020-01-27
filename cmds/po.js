f1dc7c9502280fd8913d4bbbca655e51d2354b84
gearysw <46032052+gearysw@users.noreply.github.com>
2020-01-26 22:24:02 -0800

    added more status feedback for po command

--- cmds/po.js
+++ cmds/po.js
@@ -14,6 +14,7 @@ module.exports = {
     usage: '<po number>',
     easteregg: false,
     execute: async (bot, message, args) => {
+        const searchingMsg = await message.channel.send('Searching...');
         fs.readFile('./credentials.json', (err, content) => {
             if (err) return console.log('Error loading secret: ', err);
             authorize(JSON.parse(content), readSheet);
@@ -74,7 +75,11 @@ module.exports = {
                     const poNum = rows.map(p => p[0]);
                     const lookup = poNum.indexOf(args[0]);
 
-                    if (lookup == -1) return message.channel.send('No order with that PO # found.');
+                    if (lookup == -1) {
+                        message.channel.send('No order with that PO # found.');
+                        searchingMsg.delete();
+                        return;
+                    }
 
                     const status = rows[lookup][10];
                     const PO = rows[lookup][0];
@@ -102,6 +107,7 @@ module.exports = {
                         .addField('Item Link', link);
 
                     message.channel.send(embed);
+                    searchingMsg.delete();
                 } else {
                     console.log('No data');
                 }
