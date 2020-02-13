3f1031b5a5484527ad28c38e01324b5e3ad3d334
gearysw <46032052+gearysw@users.noreply.github.com>
2020-02-13 01:27:50 -0800

    added more tutorials

--- cmds/tutorials.js
+++ cmds/tutorials.js
@@ -13,6 +13,7 @@ module.exports = {
             .addField('SolidWorks', 'https://www.solidworks.com/sw/resources/solidworks-tutorials.htm')
             .addField('LaTeX', 'https://www.latex-tutorial.com/')
             .addField('Coding', 'https://www.w3schools.com/')
+            .addField('Using Abaqus for Chassis Simulation', 'https://drive.google.com/open?id=1sS9be25Nj2IoOwkJELR1M9gVe0-DaaQNVsUQKTJNZlc')
             .setFooter('If you want more tutorials added in, contact Geary or another server developer.');
 
         message.channel.send(tutorialEmbed);
