8edec41f6c17f39df502d255aed381b7fdc7c9d2
gearysw <46032052+gearysw@users.noreply.github.com>
2020-12-02 23:16:42 -0800

    added `!pdm` command

---
+++ cmds/pdm.js
@@ -0,0 +1,19 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: 'pdm',
+    description: 'How to get up and running with PDM',
+    category: 'Team',
+    args: false,
+    showInHelp: true,
+    easteregg: false,
+    execute: async (bot, message, args) => {
+        const embed = new MessageEmbed().setTitle('Setting up PDM').setColor('#004426')
+            .setDescription(`Work through the steps in the given order. Prior to connecting to the PDM, inform a leadership member so that they can create a user account for you. If you have any questions, feel free to reach out to leadership.`)
+            .addField('1. Familiarize yourself with the parts naming scheme', 'https://drive.google.com/file/d/1V9Qpcd-OfVVbPAqEXigI4VsCuygEc3o5/view?usp=sharing')
+            .addField('2. Connect to the Vault', 'https://docs.google.com/document/d/1Qqnl0Wx0X3goQoDqMQ1wXg2nOPQApGQ1mY8Ef_BN_Hs/edit?usp=sharing')
+            .addField('3. Learn the basic workflow of PDM', 'https://youtu.be/dGfZCgigtQ4');
+
+        message.channel.send(embed);
+    }
+}
