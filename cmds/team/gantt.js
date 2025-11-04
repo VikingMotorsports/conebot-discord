b837e8058bd4ffcfd2f416c5acfff9ce7d19fdd4
ethan dibble <edibble@pdx.edu>
2025-11-03 21:28:50 -0800

    prettyfied

--- cmds/team/gantt.js
+++ cmds/team/gantt.js
@@ -12,18 +12,18 @@ const { SlashCommandBuilder } = require('discord.js');
 const { gantt } = require('../../links.json');
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('gantt')
-    .setDescription('Overall timeline of vehicle projects'),
-  aliases: ['ganttchart', 'timeline'],
-  category: 'Car',
-  showInHelp: true,
-  easteregg: false,
-  throughLinksCommand: true,
-  execute: async (_bot, _message, _args) => {
-    return gantt || 'error: field unset';
-  },
-  interact: async (interaction) => {
-    await interaction.reply(gantt || 'error: field unset');
-  },
+    data: new SlashCommandBuilder()
+        .setName('gantt')
+        .setDescription('Overall timeline of vehicle projects'),
+    aliases: ['ganttchart', 'timeline'],
+    category: 'Car',
+    showInHelp: true,
+    easteregg: false,
+    throughLinksCommand: true,
+    execute: async (_bot, _message, _args) => {
+        return gantt || 'error: field unset';
+    },
+    interact: async (interaction) => {
+        await interaction.reply(gantt || 'error: field unset');
+    },
 };
