5ac367b7409de20458a9108d999aad939c1e2d1b
Ethan Randolph Dibble <edibble@pdx.edu>
2025-11-03 21:32:07 -0800

    Feature: gantt slash command  (#57)

--- cmds/team/gantt.js
+++ cmds/team/gantt.js
@@ -3,15 +3,18 @@
  *
  * Prefix command:
  * <prefix>gantt         Return link.
+ *
+ * Slash command:
+ * /gantt                Return link.
  */
 
+const { SlashCommandBuilder } = require('discord.js');
 const { gantt } = require('../../links.json');
 
 module.exports = {
-    data: {
-        name: 'gantt',
-        description: 'Overall timeline of vehicle projects',
-    },
+    data: new SlashCommandBuilder()
+        .setName('gantt')
+        .setDescription('Overall timeline of vehicle projects'),
     aliases: ['ganttchart', 'timeline'],
     category: 'Car',
     showInHelp: true,
@@ -19,5 +22,8 @@ module.exports = {
     throughLinksCommand: true,
     execute: async (_bot, _message, _args) => {
         return gantt || 'error: field unset';
+    },
+    interact: async (interaction) => {
+        await interaction.reply(gantt || 'error: field unset');
     },
 };
