4fe0297647608458fca5a61ba29cc9f5d943c268
ethan dibble <edibble@pdx.edu>
2025-11-03 19:00:14 -0800

    make tests fail properly

--- test.js
+++ test.js
@@ -11,32 +11,36 @@ test('command interface test', () => {
     const commandInterface = {
         data: true,
         execute: true,
-        interact: true,
+        //interact: true,
         //help: true,
     };
 
     const foldersPath = path.join(__dirname, 'cmds');
-    const commandFolders = fs
-        .readdirSync(foldersPath)
-        .filter((file) => file !== 'README.md');
+    const directory = fs.readdirSync(foldersPath, { withFileTypes: true });
 
-    for (const folder of commandFolders) {
-        const commandsPath = path.join(foldersPath, folder);
-        const commandFiles = fs
-            .readdirSync(commandsPath)
-            .filter((file) => file.endsWith('.js'));
+    let failed = false;
+    for (const entry of directory) {
+        if (entry.isDirectory()) {
+            const commandsPath = path.join(foldersPath, entry.name);
+            const commandFiles = fs
+                .readdirSync(commandsPath)
+                .filter((file) => file.endsWith('.js'));
 
-        for (const file of commandFiles) {
-            const filePath = path.join(commandsPath, file);
-            const command = require(filePath);
+            for (const file of commandFiles) {
+                const filePath = path.join(commandsPath, file);
+                const command = require(filePath);
 
-            for (const field in commandInterface) {
-                try {
-                    assert(field in command);
-                } catch (_) {
-                    console.error(`missing [${field}] ${command.data.name}`);
+                for (const field in commandInterface) {
+                    if (!(field in command)) {
+                        failed = true;
+                        console.error(`missing [${field}] ${command.data.name}`);
+                    }
                 }
             }
         }
+    }
+
+    if (failed) {
+        throw 'interface test failed';
     }
 });
